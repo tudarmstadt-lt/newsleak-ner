dcfcc3950e18c61efe72ad52dd20ee76bc59f035
Gregor Wiedemann <kontakt@gregor-wiedemann.de>
2018-05-03 19:59:45 +0200

    check for installed lang models

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,19 @@
 FROM ubuntu
-RUN apt-get update && apt-get install -y python-numpy libicu-dev python-dev python-pip
+RUN apt-get update && apt-get install -y locales python-numpy libicu-dev python-dev python-pip
+
+# Set the locale
+RUN locale-gen en_US.UTF-8  
+ENV LANG en_US.UTF-8  
+ENV LANGUAGE en_US:en  
+ENV LC_ALL en_US.UTF-8
+
 WORKDIR /opt/app/
 EXPOSE 5001
 ADD requirements.txt .
 RUN pip install -r requirements.txt
-RUN polyglot download embeddings2.en embeddings2.de ner2.en ner2.de
+RUN mkdir /root/polyglot_data
+RUN polyglot download embeddings2.en ner2.en
+VOLUME /root/polyglot_data
 ADD app.py .
 ENV FLASK_APP app.py
 ENV FLASK_DEBUG 0
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify, make_response
 import polyglot.tag
+import os
+import logging
 
 app = Flask(__name__)
 
@@ -7,7 +9,32 @@ from datetime import timedelta
 from flask import make_response, request, current_app
 from functools import update_wrapper
 
-supportedLanguages = {"en", "de"}
+supportedLanguagesNer = [name for name in os.listdir("/root/polyglot_data/ner2")]
+supportedLanguagesEmbeddings = [name for name in os.listdir("/root/polyglot_data/embeddings2")]
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
 
 def crossdomain(origin=None, methods=None, headers=None,
                 max_age=21600, attach_to_all=True,
@@ -67,9 +94,10 @@ def analyze():
         try:
             docdata = request.get_json();
             lang = docdata["lang"]
-            if lang not in supportedLanguages:
-                print("Unknown language: " + lang + ". Switching to en as default.")
-                lang = "en"
+            if lang not in supportedLanguagesNer:
+                raise InvalidUsage("No ner2 model for: " + lang, status_code=404)
+            if lang not in supportedLanguagesEmbeddings:
+                raise InvalidUsage("No embeddings2 model for: " + lang, status_code=404)
             chunker = polyglot.tag.NEChunker(lang = lang)
 
             result = []
